94fa23acb55aa556c3b6fd1f00316efe35c7bc83
sfleta <sfleta@student.21-school.ru>
2022-12-06 04:30:45 +0700

    done

--- practicum.sql
+++ practicum.sql
@@ -29,18 +29,54 @@ group by first_name
 having count > 1;
 
 -- 5 Вывести фамилию, имя, контактные данные студента_ки - который_ая посетил_а больше всего уроков.
-select first_name, last_name, email, phone, count() as count
+select first_name, last_name, email, phone
 from persons p
 join contacts c on p.person_id = c.persons_id
 join person_lesson pl on p.person_id = pl.person_id
 join lessons l on l.lesson_id = pl.lesson_id
 group by p.person_id
-order by count desc
-limit 1;
-
+having count() = (select max(count) from (select count() as count
+                                            from persons p
+                                            join contacts c on p.person_id = c.persons_id
+                                            join person_lesson pl on p.person_id = pl.person_id
+                                            join lessons l on l.lesson_id = pl.lesson_id
+                                            group by p.person_id)
+);
 
 -- 6 Вывести фамилию, имя, контактные данные учителя - который потратил больше всего времени на уроках.
+select last_name, first_name, email, phone
+from persons p
+join contacts c on p.person_id = c.persons_id
+join person_lesson pl on p.person_id = pl.person_id
+join lessons l on l.lesson_id = pl.lesson_id
+where role='teacher'
+group by p.person_id
+having sum(time) = (select max(count) from (select sum(time) as count
+                                            from persons p
+                                            join contacts c on p.person_id = c.persons_id
+                                            join person_lesson pl on p.person_id = pl.person_id
+                                            join lessons l on l.lesson_id = pl.lesson_id
+                                            where role='teacher'
+                                            group by p.person_id));
+
 -- 7 Вывести для каждого_ой студента_ки (фамилия, имя) рекомендуемые материалы всех пройденных конкретным студентом_кой уроков.
+select last_name, first_name, group_concat(material_name, char(13) || char(10)) as materials
+from persons
+join person_lesson pl on persons.person_id = pl.person_id
+join lessons l on l.lesson_id = pl.lesson_id
+join material_lesson ml on l.lesson_id = ml.lesson_id
+join material m on m.material_id = ml.material_id
+where role='student'
+group by 1,2;
+
 -- 8 Вывести для каждого_ой студента_ки (фамилия, имя) уникальные рекомендуемые материалы (они не должны повторяться) всех пройденных конкретным студентом_кой уроков.
+-- select last_name, first_name, (select DISTINCT material_name from material m where m.material_id = ml.material_id) as materials
+-- from persons
+-- join person_lesson pl on persons.person_id = pl.person_id
+-- join lessons l on l.lesson_id = pl.lesson_id
+-- join material_lesson ml on l.lesson_id = ml.lesson_id
+-- where role='student'
+-- group by 1,2;
+
 -- 9 Вывести страну в которой живут студенты посетившие больше всего уроков.
 -- 10 Вывести город в котором живут студенты которые потратили больше всего время на посещения уроков.
